69bbdc7ba2bac4fe3495ca1e5c4297592dbea27f
Ana Paula Oliva <anapaulafernandez.oliva@gmail.com>
2020-03-11 14:26:55 -0600

    Require extractLinks module

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const { readFileToString } = require('./scripts/readFileToString.js');
+const { extractLinks } = require('./scripts/extractLinks.js');
 
 const mdLinks = (path, options = { validate: false, stats: false }) => {
 
     const promise = new Promise((resolve, reject) => {
         readFileToString(path)
-        .then(data => {
-            resolve(data);
+        .then((response) => {
+            return extractLinks(response, path)
+        
+        .then(response => {
+
+            resolve(response)
         })
         .catch(reject);
-    })
+        })
     return promise;
+    })
 }
 
 module.exports = { mdLinks };
