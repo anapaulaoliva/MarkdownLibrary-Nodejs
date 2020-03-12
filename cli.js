49d4e29605af93cfb99d93053eb6ba49b29a7822
Ana Paula Oliva <anapaulafernandez.oliva@gmail.com>
2020-03-12 09:49:37 -0600

    Functionality of cli.js executable

--- cli.js
+++ cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const { mdlinks } = require('./index.js');
+const { mdLinks } = require('./index.js');
 
 const path = process.argv[2];
 const options = {
@@ -8,7 +8,7 @@ const options = {
     stats: process.argv.includes('--stats') || process.argv.includes('-s')
 }
 
-mdlinks('./README.md', {validate: false, stats: false}).then((data) => {
+mdLinks(path, {validate: false, stats: false}).then((data) => {
     console.log(data);
     
 }).catch((err) => {
