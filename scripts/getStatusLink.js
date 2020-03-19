7874203bc3b8e8be62d72ed7f95f98431c2a1f6d
Ana Paula Oliva <anapaulafernandez.oliva@gmail.com>
2020-03-19 12:30:21 -0600

    Returns an array of objects with fetch request

--- scripts/getStatusLink.js
+++ scripts/getStatusLink.js
@@ -1,7 +1,49 @@
 const fetch = require('node-fetch');
 
-module.exports.getStatusLink = (arrayLinks) => {
+module.exports.getStatusLink = (array) => {
+    //Debe recibir un array de Links
+    return new Promise((resolve, reject)=> {
+        const newArray =array.map( (element) => {
+            return new Promise((resolve, reject)=> {
+                
+                fetch(element.href)
+                .then((res) => {
+                    let object = {
+                        Href: "", 
+                        Status: ""
+                        };
 
+                    if(res.status >= 200 && res.status < 300){
+                        object.Href = res.url;
+                        object.Text = element.text;
+                        object.Status = res.status;
+                        object.StatusText = res.statusText;
+
+                        resolve(object);
+                    } else {
+
+                        object.Href = res.url;
+                        object.Text = element.text;
+                        object.Status = res.status;
+                        object.StatusText = res.statusText;
+
+                        resolve(object);
+                    }
+                })
+                .catch((err)=>{
+                    reject("No se pudo realizar la peticion al link", err)
+                })
+            })
+        })        
+        Promise.all(newArray).then((res)=> {
+            //console.log("esto queremos devolvers",res);
+            resolve(res);
+        
+        })
+    });
+
+}
+    /*
     let promises = [];
 
     for (let item of pathInfoArray) {
@@ -36,4 +78,4 @@ module.exports.getStatusLink = (arrayLinks) => {
                 console.log(err));
                     
     return validatePromise;
-}
+} */
